4b5352887025e51bde80c929eb74e5f24c2ec40d
Sanjarov17 <ssanjarov870@gmail.com>
2025-09-10 20:19:07 +0500

    gg

--- practice11.py
+++ practice11.py
@@ -1,18 +1,1 @@
-def calculate_tax(salary: float) -> float:
-    if salary > 5_000_000:
-        tax = salary * 0.20
-    else:
-        tax = salary * 0.13
-    return tax
 
-def calculate_net_salary(salary: float) -> float:
-    tax = calculate_tax(salary)
-    return salary - tax
-
-salary = float(input("Maoshingizni kiriting: "))
-
-tax = calculate_tax(salary)
-net = calculate_net_salary(salary)
-
-print(f"Soliq miqdori: {tax:.2f} so'm")
-print(f"Toza maosh (soliqdan keyin): {net:.2f} so'm")
--- practice13.py
+++ practice13.py
@@ -1,10 +1,1 @@
-def is_palindrome(text: str) -> bool:
-    return text == text[::-1]
 
-
-soz = input("So'zni kiriting: ")
-
-if is_palindrome(soz):
-    print(" Bu so'z palindrom!")
-else:
-    print(" Bu so'z palindrom emas.")
